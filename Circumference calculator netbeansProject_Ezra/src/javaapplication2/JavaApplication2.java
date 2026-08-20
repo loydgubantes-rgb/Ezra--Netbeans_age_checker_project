1f6817a56c46ab12cf334e803adb21930b49879c
loydgubantes-rgb <loydgubantes@gmail.com>
2026-08-20 22:27:42 +0800

    Completed the Circumference calculator

---
+++ Circumference calculator netbeansProject_Ezra/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author SkywereDaSharj
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the radius of a circle");
+    double radius = input.nextDouble();
+    
+    
+    if (radius <= 0) {
+    System.out.println("Invalid radius");
+    } else {
+    double area = Math.PI * Math.pow(radius, 2);
+    double circumference = 2 * Math.PI * radius;
+    System.out.println("The area is: " + area);
+    System.out.println("Circumference: " + circumference);
+    
+}
+
+    }
+    
+}
